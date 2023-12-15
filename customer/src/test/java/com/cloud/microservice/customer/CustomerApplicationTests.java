325c4f395214bcf9d259212016f12045101591a1
amirparvaneh <amirparvaneh138@gmail.com>
2023-12-15 14:18:55 +0330

    first commit

---
+++ customer/src/test/java/com/cloud/microservice/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.microservice.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
